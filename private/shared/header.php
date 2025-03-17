e1734ae298cfe822f3d4c2fe2d267d0babf9cf1d
Rakich <20134@rosmini.school.nz>
2025-03-18 11:37:22 +1300

    Write a message

---
+++ private/shared/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="/public/stylesheet/styles.css">
+<html lang="en">
+<div class="navbar">
+	<nav>
+  		<a href="http://localhost/public/index.php">Home</a>
+        <a href="http://localhost/public/help.php">Help</a>
+        <a href="http://localhost/public/info.php">Info</a>
+	</nav>
+</div>
+</html>
