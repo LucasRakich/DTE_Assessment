de1e0425757de6f701e08807328956f92b76664b
Rakich <20134@rosmini.school.nz>
2025-03-18 11:39:01 +1300

    Write a message

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<link rel="stylesheet" href="/public/stylesheet/styles.css">
+<?php require_once("../private/initialize.php")?>
+<?php include (SHARED_PATH . "/header.php")?>
+
+<?php echo "Hi" ?>
