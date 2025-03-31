1880340903d87ff06e58ec9dd1f7376fccb9951a
Rakich <20134@rosmini.school.nz>
2025-03-31 13:57:56 +1300

    Write a message

---
+++ private/shared/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="/public/stylesheet/styles.css">
+<footer>
+	<h6>Contact Us</h6>
+	<h6>36 Dominion Street</h6>
+	<h6>Takapuna</h6>
+	<h6>Auckland, 0622</h6>
+	<h6>Phone: +64 9 489 5417</h6>
+	<h6>admin@rosmini.school.nz</h6>
+	<div>
+    <nav class="Navfoot">
+            <a href="http://localhost/public/index.php">Home</a>
+            <a href="http://localhost/public/help.php">Help</a>
+            <a href="http://localhost/public/info.php">Info</a>
+        </nav>
+	</div>
+</footer>
+</html>
--- private/shared/header.php
+++ private/shared/header.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="../public/stylesheet/styles.css">
+<link rel="stylesheet" href="/public/stylesheet/styles.css">
 <html lang="en">
-<div>
-	<nav class="Navbar">
-  		<a href="http://localhost/public/index.php">Home</a>
-        <a href="http://localhost/public/help.php">Help</a>
-        <a href="http://localhost/public/info.php">Info</a>
-	</nav>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Zealand Housing Crisis</title>
+</head>
+<body>
+    <div class="Navdiv">
+    <img src="https://prodcdn.sporty.co.nz/cms/11319/71566/1430582/bf943936-2268-41af-a752-0743089ef9b5_wo.png?t=638168090966270000" alt="RosminiLogo">
+        <nav class="Navbar">
+            <p><a href="http://localhost/public/index.php">Home</a></p>
+            <p><a href="http://localhost/public/help.php">Contact Us</a></p>
+            <p><a href="http://localhost/public/info.php">FAQ</a></p>
+        </nav>
+    </div>
+</body>
 </html>
